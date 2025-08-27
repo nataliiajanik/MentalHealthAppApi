49c7727ff91d693770d39b74c6503407660ae12d
Natalia Janik <nataliia.janik@gmail.com>
2025-08-27 19:38:15 +0200

    Dodaj pliki projektów.

---
+++ MentalHealthAppApi/Controllers/DiseasePredictionController.cs
@@ -0,0 +1,36 @@
+﻿using MentalHealthAppApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MentalHealthAppApi.Controllers
+{
+    [ApiController]
+    [Route("api/diseasePrediction")]
+    public class DiseasePredictionController : ControllerBase
+    {
+        //sesja z załadowanym modelem ONNX aby można było go wielokrotnie wyłowywać bez osobnego załadowywania
+        private readonly DiseasePredictionModelService _modelService;
+
+        public DiseasePredictionController(DiseasePredictionModelService modelService)
+        {
+            _modelService = modelService;
+        }
+
+        [HttpPost]
+        public IActionResult PredictDisease([FromBody] int[] answers)
+        {
+            //IActionResult rezultat akcji kontrolera
+            try
+            {
+                var predictionProbabilites = _modelService.PredictProbability(answers)
+                    .Select(p => p * 100)
+                    .ToArray();
+
+                return Ok( new { predictionProbabilites });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ MentalHealthAppApi/Services/DiseasePredictionModelService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+
+namespace MentalHealthAppApi.Services
+{
+    public class DiseasePredictionModelService
+    {
+        //inicjalizacja sesji, potem ładowanie modelu ONNX do pamięci i wykonywanie na nim predykcji
+        private readonly InferenceSession _inferenceSession;
+        public DiseasePredictionModelService(string modelPath) 
+        {
+            //ładywanie modelu ONNX
+            _inferenceSession = new InferenceSession(modelPath);
+        }
+
+        public float[] PredictProbability(int[] answers)
+        {
+            if(answers.Length != 51)
+            {
+                throw new ArgumentException("Prediction expects 51 answers.");
+            }
+
+            // wielowymiarowy tensor, wejście do modelu predykcyjnego ONNX
+            // DenseTensor przechowuje dane w formacie, które ONNX potrafi odczytać
+            var inputTensor = new DenseTensor<int>(answers, new int[] { 1, 51 });
+
+            // tworzy NamedOnnxValue z inputTensor i nazwy jaką ma input w modelu ONNX
+            //NamedOnnxValue połączona nazwa + wartość wejścia/wyjścia
+            //musi być listą bo Run w InferenceSession przyjmuje listę IEnumerable<NamedOnnxValue>
+            var inputsOnnx = new List<NamedOnnxValue> {
+                NamedOnnxValue.CreateFromTensor("input", inputTensor)
+            };
+
+            //uruchomienie predykcji
+            using var results = _inferenceSession.Run(inputsOnnx);
+
+            // results zawiera jedno wyjście, którt składa się z 5ciu prawdopodobieństw dla 5 klas
+            // konwersja tensora do płaskiej listy
+            var predictedProbabilities = results.First().AsEnumerable<float>().ToArray();
+
+            return predictedProbabilities;
+        }
+    }
+}
+
